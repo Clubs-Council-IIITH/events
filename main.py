c184c0ab9eff30247f274c3b88748defe1e8ae7d
v15hv4 <vishva2912@gmail.com>
2023-04-28 22:01:51 +0530

    Disable graphiql on prod

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from strawberry.fastapi import GraphQLRouter
 
 from fastapi import FastAPI
 
+from os import getenv
+
 # override PyObjectId and Context scalars
 from models import PyObjectId
 from otypes import Context, PyObjectIdType
@@ -13,6 +15,8 @@ from otypes import Context, PyObjectIdType
 from queries import queries
 from mutations import mutations
 
+# check whether running in debug mode
+DEBUG = getenv("SERVICES_DEBUG", "False").lower() in ("true", "1", "t")
 
 # create query types
 Query = create_type("Query", queries)
@@ -34,6 +38,10 @@ schema = strawberry.federation.Schema(
 )
 
 # serve API with FastAPI router
-gql_app = GraphQLRouter(schema, context_getter=get_context)
-app = FastAPI()
+gql_app = GraphQLRouter(
+    schema, 
+    graphiql=True,
+    context_getter=get_context
+)
+app = FastAPI(debug=DEBUG)
 app.include_router(gql_app, prefix="/graphql")
