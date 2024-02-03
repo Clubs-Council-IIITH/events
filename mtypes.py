996a8a0e7f038e80c231db592bae8ca93650f798
Bhav Beri <bhavberi@gmail.com>
2024-02-03 17:46:51 +0530

    Increase Event textfield lengths

--- mtypes.py
+++ mtypes.py
@@ -122,7 +122,7 @@ class Event_Location(StrEnum):
     other = auto()
 
 
-event_name_type = Annotated[str, StringConstraints(min_length=1, max_length=100)]
+event_name_type = Annotated[str, StringConstraints(min_length=1, max_length=200)]
 event_desc_type = Annotated[str, StringConstraints(max_length=5000)]
 event_popu_type = Annotated[int, Field(ge=0)]
 event_othr_type = Annotated[str, StringConstraints(max_length=1000)]
