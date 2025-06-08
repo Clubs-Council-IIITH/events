7d5d5b30030aadaa2f0fc280273c53a3623c95c4
Bhav Beri <bhavberi@gmail.com>
2025-06-08 11:40:00 +0530

    Formatted the member no img file

--- scripts/member_no_img.py
+++ scripts/member_no_img.py
@@ -1,7 +1,8 @@
-from os import getenv
-from datetime import datetime, timezone
-from pymongo import MongoClient
 import csv
+from datetime import datetime
+from os import getenv, makedirs
+
+from pymongo import MongoClient
 
 MONGO_URI = "mongodb://{}:{}@mongo:{}/".format(
     getenv("MONGO_USERNAME", default="username"),
@@ -9,15 +10,10 @@ MONGO_URI = "mongodb://{}:{}@mongo:{}/".format(
     getenv("MONGO_PORT", default="27107"),
 )
 MONGO_DATABASE = getenv("MONGO_DATABASE", default="default")
-# MongoDB connection
-# MONGO_URI = "mongodb://{}:{}@localhost:{}/".format(
-#     getenv("MONGO_USERNAME", default="username"),
-#     getenv("MONGO_PASSWORD", default="password"),
-#     getenv("MONGO_PORT", default="27017"),
-# )
-# MONGO_DATABASE = 'dev'
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DATABASE]
+
+makedirs("reports", exist_ok=True)
 
 current_year = datetime.now().year
 # Fetch active clubs
@@ -27,31 +23,34 @@ userlist = []
 
 # Process each club
 for club in clubs:
-    if club['state'] != "active":
+    if club["state"] != "active":
         continue
-    results = db.members.find({"cid": club['cid']})
+    results = db.members.find({"cid": club["cid"]})
     members = [result for result in results]
     for member in members:
         flag = 0
-        for role in member['roles']:
-            if role['end_year'] == None or int(role['end_year']) >= current_year:
+        for role in member["roles"]:
+            if (
+                role["end_year"] is None
+                or int(role["end_year"]) >= current_year
+            ):
                 flag = 1
                 break
         if flag == 1:
-            users = db.users.find({"uid": member['uid']})
+            users = db.users.find({"uid": member["uid"]})
             users = [user for user in users]
             for user in users:
-                if user['img'] == None:
-                    if user['uid'] not in userlist:
-                        userlist.append(user['uid'])
+                if user["img"] is None:
+                    if user["uid"] not in userlist:
+                        userlist.append(user["uid"])
                     break
 
 # Write results to a CSV file
-with open("members_without_images.csv", "w", newline="") as csvfile:
+with open("reports/members_without_images.csv", "w", newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
 
     # Write the header
-    csvwriter.writerow(["User Name"])
+    csvwriter.writerow(["User UID"])
 
     # Write the data
     for users in userlist:
